5a59b25a00afdc4a07e5cf04a7e8a3c7df32c3a9
Miron Pawlik <miron.pawlik@swmansion.com>
2025-02-03 21:35:54 +0100

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 import { MasonryPhotoAlbum } from "react-photo-album";
-// import "react-photo-album/rows.css";
 import "react-photo-album/masonry.css";
 
 import Lightbox from "yet-another-react-lightbox";
@@ -17,7 +16,6 @@ import photos from "./photos";
 
 export default function App() {
   const [index, setIndex] = useState(-1);
-  console.log("asdf");
 
   return (
     <>
@@ -27,23 +25,7 @@ export default function App() {
         columns={3}
         photos={photos}
         onClick={({ index }) => setIndex(index)}
-        render={{
-          wrapper: ({ style, ...rest }) => (
-            <div
-              style={{
-                ...style,
-                borderRadius: "4px",
-                boxShadow:
-                  "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
-                transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-              }}
-              data-asdf={console.log("dupa", rest)}
-              {...rest}
-            />
-          ),
-        }}
       />
-      xd
       <Lightbox
         slides={photos}
         open={index >= 0}

d4c60afb0e965a134cccdb636f3d2d19c77c3297
Miron Pawlik <miron.pawlik@swmansion.com>
2025-02-03 22:13:39 +0100

    Small imprvmnts

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import ytlogo from "./assets/ytlogo.png";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <header>
-      <h1>Pan Jamnik - Galeria</h1>
-      <a href="https://www.youtube.com/@panjamnik" target="_blank">
-        YouTube
-      </a>
+    <header style={{ maxWidth: 1200, justifyContent: "space-between", marginLeft: "auto", marginRight: "auto" }}>
+      <div>
+        <h1>Pan Jamnik - Galeria</h1>
+      </div>
+      <div>
+        <a href="https://www.youtube.com/@panjamnik" target="_blank">
+          <img src={ytlogo} alt="Logo" height={30} />
+        </a>
+      </div>
     </header>
     <main>
       <App />

408181d1cbc5ecceae447396d082faa56d067195
Miron <miron@ik.me>
2025-09-28 15:24:58 +0200

    Naucz się pisać

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
           src: 'nereczka',
           dest: 'nereczka'
         },
+        {
+          src: 'przepisy',
+          dest: 'przepisy'
+        },
       ],
     }),],
 
